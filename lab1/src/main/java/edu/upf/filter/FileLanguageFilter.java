0da038d7c9a5ec9deebc0386c2b46e6522b8d694
Anni0401 <72462499+Anni0401@users.noreply.github.com>
2023-01-29 18:02:17 +0100

    Add files via upload

---
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -0,0 +1,44 @@
+package edu.upf.filter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Optional;
+
+import edu.upf.parser.SimplifiedTweet;
+
+public class FileLanguageFilter implements LanguageFilter{
+    final String inputFile;
+    final String outputFile;
+
+    public FileLanguageFilter (String inputFile, String outputFile){
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+    @Override
+    public void filterLanguage(String language) throws Exception {
+        try(FileReader fr = new FileReader(inputFile);
+            BufferedReader br = new BufferedReader(fr);
+            FileWriter fw = new FileWriter(outputFile);
+            BufferedWriter bw = new BufferedWriter(fw);) {
+                
+                String jsonLine = "1";
+                while ((jsonLine = br.readLine()) != null){
+                System.out.println(jsonLine);
+                Optional<SimplifiedTweet> optionalTweet =  SimplifiedTweet.fromJson(jsonLine);
+                if (optionalTweet.isPresent()){
+                SimplifiedTweet tweet = optionalTweet.get();
+                if (tweet.getLanguage().equals(language)){
+                    bw.write(tweet.toString());
+
+                }
+            }
+        }
+
+    }
+        
+    }
+
+    
+}
