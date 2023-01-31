b1e441c9af1504971cd6145c2bd41243a990df31
Carlamorah <carla.mora01@estudiant.upf.edu>
2023-01-31 13:32:37 +0100

    j

--- lab1/src/main/java/edu/upf/uploader/S3Uploader.java
+++ lab1/src/main/java/edu/upf/uploader/S3Uploader.java
@@ -2,9 +2,39 @@ package edu.upf.uploader;
 
 import java.util.List;
 
-public class S3Uploader implements Uploader {
-    @Override
-    public void upload(List<String> files) {
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
+/*Don't know how to initialize an Amazon client using AWS -> ~/.aws/credentials]
+[default]
+    aws_access_key_id = YOUR_ACCESS_KEY
+    aws_secret_access_key = YOUR_SECRET_KEY
+
+    System.setProperty("aws.accessKeyId", "YOUR_ACCESS_KEY");
+    System.setProperty("aws.secretKey", "YOUR_SECRET_KEY");
+*/
+
+
+public class S3Uploader implements Uploader {
+
+    final String inputFile;
+    final String bucketName;
+    final String prefix;
+    private AmazonS3 s3Client;
+
+    public S3Uploader(String inputFile, String bucketName, String prefix){
+        this.inputFile = inputFile;
+        this.bucketName = bucketName;
+        this.prefix = prefix;
+        this.s3Client = AmazonS3ClientBuilder.defaultClient();
+    }
+    @Override
+
+    public void upload(List<String> files) {
+        for (String file:files){
+            PutObjectRequest request = new PutObjectRequest(bucketName, prefix, file);
+            s3Client.putObject(request);
+        }
     }
 }
